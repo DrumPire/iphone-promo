56f671f801d8ff4a2fad9385ada18f831ac5e22e
DrumPire <expresproject@gmail.com>
2022-01-22 18:28:49 +0200

    added tabs on card

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,36 +1,34 @@
-const links = document.querySelectorAll('.header-menu__item a');
-const linkCharacteristics = document.querySelector('.card-details__link-characteristics');
+const scrollFunction = () => {
 
-seamless.polyfill();
-
-links.forEach(item => {
-  item.addEventListener('click', e => {
-    e.preventDefault();
-
-    const id = item.getAttribute('href').substring(1);
-    const section = document.getElementById(id);
-
-    if (section) {
-      seamless.elementScrollIntoView(section, {
-        behavior: 'smooth',
-        block: 'start'
-      });
-    } else {
-      seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
-        behavior: "smooth",
-        block: "center",
-        inline: "center",
+  const links = document.querySelectorAll('.header-menu__item a');
+  const linkCharacteristics = document.querySelector('.card-details__link-characteristics');
+  
+  const newArray = [...links, linkCharacteristics];
+  
+  seamless.polyfill();
+  
+  newArray.forEach(item => {
+    item.addEventListener('click', e => {
+      e.preventDefault();
+  
+      const id = item.getAttribute('href').substring(1);
+      const section = document.getElementById(id);
+  
+      if (section) {
+        seamless.elementScrollIntoView(section, {
+          behavior: 'smooth',
+          block: 'start'
+        });
+      } else {
+        seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+          behavior: "smooth",
+          block: "center",
+          inline: "center",
+        });
+      }
     });
-    }
   });
-});
 
-linkCharacteristics.addEventListener('click', e => {
-  e.preventDefault();
+};
 
-  seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
-    behavior: "smooth",
-    block: "center",
-    inline: "center",
-  });
-});
+scrollFunction();
